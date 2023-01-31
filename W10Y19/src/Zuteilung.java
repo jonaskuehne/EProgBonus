7bfc0d9ade5e0a952d51192f09f0db1166560145
jonaskuehne <jonas.kuehne@proton.me>
2023-01-31 16:10:35 +0100

    clean

--- W10Y19/src/Zuteilung.java
+++
@@ -1,7 +0,0 @@
-
-interface Zuteilung {
-	// Returned ob die Zuteilung korrekt ist
-	boolean istKorrekt();
-	// Passt die Zuteilung an, falls die Zuteilung noch nicht korrekt ist
-	void anpassen();
-}
